34781575c4ac1636de3f1d5ee9e13d24babe6c70
George Peng <george20000plus@yahoo.com.tw>
2019-08-04 19:20:27 +0800

    Adds the forgot-password form
    Also,
    1. Sets auto focus
    2. Sets required form input attribute

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ def handle_get_index():
     return render_template('index.html')
 
 
+@app.route('/forgotpass', methods=['GET'])
+def handle_get_forgotpass():
+    return render_template('forgotpass.html')
+
+
 @app.route('/chpass', methods=['GET'])
 def handle_get_chpass():
     return render_template('chpass.html')
